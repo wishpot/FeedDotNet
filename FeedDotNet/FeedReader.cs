fc622c70e30df55b66d965c7f12b18de87a14134
Tom Lianza <tom@wishpot.com>
2011-12-28 19:29:30 -0800

    [Fixes #22506847] Use a caching xmlresolver to limit the number of web requests we make to validate DTDs.

--- FeedDotNet/FeedReader.cs
+++ FeedDotNet/FeedReader.cs
@@ -95,11 +95,8 @@ namespace FeedDotNet
       /// <returns></returns>
         public static Feed Read(Stream stream, String uri, FeedReaderSettings settings)
         {
-          XmlReader xmlReader = null;
-                XmlReaderSettings xmlSettings = new XmlReaderSettings();
-                xmlSettings.ProhibitDtd = false;
-                xmlReader = XmlReader.Create(stream, xmlSettings);
-                Parser parser = null;
+          XmlReader xmlReader = XmlReader.Create(stream, new XmlReaderSettings() { DtdProcessing = DtdProcessing.Ignore, XmlResolver = new XmlCachingResolver(true) });
+          Parser parser = null;
           try{
                 Feed feed = initialize(xmlReader);
                 if (feed != null)
@@ -281,5 +278,55 @@ namespace FeedDotNet
             errors.Clear();
             return returnList;
         }
+    }
+
+    /// <summary>
+    /// Caching resolver documented here: http://msdn.microsoft.com/en-us/library/bb669135.aspx
+    /// </summary>
+    class XmlCachingResolver : XmlUrlResolver
+    {
+      bool enableHttpCaching;
+      ICredentials credentials;
+
+      //resolve resources from cache (if possible) when enableHttpCaching is set to true
+      //resolve resources from source when enableHttpcaching is set to false 
+      public XmlCachingResolver(bool enableHttpCaching)
+      {
+        this.enableHttpCaching = enableHttpCaching;
+      }
+
+      public override ICredentials Credentials
+      {
+        set
+        {
+          credentials = value;
+          base.Credentials = value;
+        }
+      }
+
+      public override object GetEntity(Uri absoluteUri, string role, Type ofObjectToReturn)
+      {
+        if (absoluteUri == null)
+        {
+          throw new ArgumentNullException("absoluteUri");
+        }
+        //resolve resources from cache (if possible)
+        if (absoluteUri.Scheme == "http" && enableHttpCaching && (ofObjectToReturn == null || ofObjectToReturn == typeof(Stream)))
+        {
+          WebRequest webReq = WebRequest.Create(absoluteUri);
+          webReq.CachePolicy = new HttpRequestCachePolicy(HttpRequestCacheLevel.Default);
+          if (credentials != null)
+          {
+            webReq.Credentials = credentials;
+          }
+          WebResponse resp = webReq.GetResponse();
+          return resp.GetResponseStream();
+        }
+        //otherwise use the default behavior of the XmlUrlResolver class (resolve resources from source)
+        else
+        {
+          return base.GetEntity(absoluteUri, role, ofObjectToReturn);
+        }
+      }
     }
 }
